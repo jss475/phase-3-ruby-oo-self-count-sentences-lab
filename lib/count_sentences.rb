8bdbf51772781789012fa00d428a2fa146becef0
JosephShin <joespam9999@gmail.com>
2022-05-17 16:42:28 -0400

    commit tree 3e4ed63e56967ac2690f602481ac53fee227c9b3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == '.'
+      true
+    else
+      false
+    end
   end
-
+ 
   def question?
-
+    if self[-1] == '?'
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == '!'
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    no_empty = []
+    split_up = self.split(/[.!?]/)
+    split_up.each do |line|
+      if(line != '')
+        no_empty.push(line)
+      end
+    end
+    no_empty.count
   end
+
 end
